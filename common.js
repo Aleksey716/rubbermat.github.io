2c0fd23868fe941177fab80a497a478b980b0e1f
Aleksey716 <72882306+Aleksey716@users.noreply.github.com>
2021-02-04 20:00:10 +0400

    Update common.js

--- common.js
+++ common.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
 
 	$("#form").submit(function() {
 		$.ajax({
+  url: "/api/getWeather",
+  data: {
+    zipcode: 97201
+  },
+  success: function( result ) {
+    $( "#weather-temp" ).html( "<strong>" + result + "</strong> degrees" );
+  }
+});
+		$.ajax({
 			type: "POST",
 			url: "mail.php",
 			data: $(this).serialize()
