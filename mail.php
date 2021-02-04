517d483b19bf6e43ff6d65a8796c54b6d2c117b2
Aleksey716 <72882306+Aleksey716@users.noreply.github.com>
2021-02-04 18:27:15 +0400

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,35 +1,12 @@
-<meta http-equiv='refresh' content='3; url=https://aleksey716.github.io/rubbermat.github.io/'>
-<meta charset="UTF-8" />
 <?php
 
-	if (isset($_POST['name']) && $_POST['name'] != "")//если существует атрибут NAME и он не пустой то создаем переменную для отправки сообщения
-		$name = $_POST['name'];
-	else die ("Не заполнено поле \"Имя\"");//если же атрибут пустой или не существует то завершаем выполнение скрипта и выдаем ошибку пользователю.
+$recepient = "agragregra@ya.ru";
+$sitename = "Название сайта";
 
-	if (isset($_POST['email']) && $_POST['email'] != "") //тут все точно так же как и в предыдушем случае
-		$email = $_POST['email'];
-	else die ("Не заполнено поле \"Email\"");
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
 
-	if (isset($_POST['subjects']) && $_POST['subjects'] != "") 
-		$sub = $_POST['subjects'];
-	else die ("Не заполнено поле \"Тема\"");
-
-	if (isset($_POST['message']) && $_POST['message'] != "") 
-		$body = $_POST['message'];
-	else die ("Не заполнено поле \"Сообщение\"");
-	 
-
-
-	$address = "Karadulev.aleksei@mail.ru";//адрес куда будет отсылаться сообщение для администратора
-	$mes  = "Имя: $name \n";	//в этих строчках мы заполняем текст сообщения. С помощью опрератора .= мы просто дополняем текст в переменную
-	$mes .= "E-mail: $email \n";
- 	$mes .= "Тема: $sub \n";
- 	$mes .= "Текст: $body"; 
-	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");//собственно сам вызов функции отправки сообшения на сервере
-
-	if ($send) //проверяем, отправилось ли сообщение
-		echo "Сообщение отправлено успешно! Перейти на <a href='https://You-hands.ru/'>you-hands.ru</a>, если вас не перенаправило вручную.";
-	else 
-		echo "Ошибка, сообщение не отправлено! Возможно, проблемы на сервере";
-		 
-?>
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
